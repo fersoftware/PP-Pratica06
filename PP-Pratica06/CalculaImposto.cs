d0b68bb7e619e74e7ec80c1250506e469f2f833a
FERNANDO ALVES DA SILVA <fersoftware@gmail.com>
2016-05-09 16:06:24 -0300

    Add project files.

---
+++ PP-Pratica06/CalculaImposto.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Escreva o método CalculaImposto, que recebe o valor da renda líquida anual e o número
+ * de dependentes de um contribuinte. Este método deverá calcular e exibir o valor do
+ * imposto a ser pago. Considere os seguintes valores:
+ * Renda líquida até R$ 1,100.00        - Isento de imposto
+ * Acima de R$ 1100,00 até R$ 5000,00   - 10 % de imposto
+ * Acima de R$ 5000,00 até R$ 10000,00  - 15% de imposto
+ * Acima de R$ 10000,00                 - 20% de imposto
+ * Cada dependente que o contribuinte possuir corresponderá a um desconto de R$ 300,00
+ * no valor do imposto. Escreva o método Main que solicita que o usuário digite os valores
+ * para contribuintes diferentes e chama o método CalculaImposto para exibir o valor do * imposto a ser pago para cada um deles.
+ */
+namespace PP_Pratica06
+{
+    class CalculaImposto
+    {
+        public void exibir()
+        {
+            Console.Write("Digite a renda liquida anual: ");
+            float rendLiquid = float.Parse(Console.ReadLine());
+            Console.Write("Digite quantos depedentes possui: ");
+            int qtdDepend = int.Parse(Console.ReadLine());
+            calculaImposto(rendLiquid, qtdDepend);
+        }
+        public void calculaImposto(float rd, int qtd)
+        {
+            float imposto = 0, desconto = 0;
+
+            if(rd <= 1100)
+            {
+                imposto = 0;
+                Console.WriteLine("Isento de imposto");
+            }
+            else if (rd > 1100 && rd <= 5000)
+            {
+                imposto = rd * 1.10f;
+                Console.WriteLine("10% de imposto -> {0:C}", imposto);
+            }
+            else if (rd > 5000 && rd <= 10000)
+            {
+                imposto = rd * 1.15f;
+                Console.WriteLine("15% de imposto -> {0:C}", imposto);
+            }
+            else if (rd > 10000)
+            {
+                imposto = rd * 1.20f;
+                Console.WriteLine("20% de imposto -> {0:C}", imposto);
+            }
+            for (int i = 0; i < qtd; i++)
+            {
+                desconto += 300;
+            }
+
+            Console.WriteLine("Depedentes: {0} -> Desconto: {1:C}", qtd, desconto);
+            Console.WriteLine("Valor a ser pago: {0:C}", imposto - desconto);
+        }
+    }
+}
---
+++ PP-Pratica06/CalculaMediaPar.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Escreva um método CalculaMediaPares, que recebe um valor inteiro n, maior do que 5.
+ * Este método deverá solicitar que o usuário digite n valores inteiros, calcular a média dos
+ * números pares digitados e exibir esse média. Escreva o método Main que deverá solicitar
+ * que o usuário digite um valor maior do que 5, chama o método CalculaMediaPares,
+ * passando o valor digitado pelo usuário.
+ */
+
+namespace PP_Pratica06
+{
+    class CalculaMediaPar
+    {
+        public void exibir()
+        {            
+            Console.Write("Digite um Valor inteiro Maior que 5:");
+            int n = int.Parse(Console.ReadLine());
+            
+            if(n > 5)
+            {
+                CalculaMediaPares(n);
+            }
+            else
+            {
+                Console.WriteLine("Valor Incorreto");
+            }            
+        }
+
+        public void CalculaMediaPares(int n)
+        {
+            int soma = n % 2 == 0 ? n : 0, qtd = n % 2 == 0 ? 1 : 0;
+            string par = n % 2 == 0 ? n + "," : "", digitados = n + ",";
+            
+            while (n > 5)
+            {                
+                Console.Write("Digite um Valor inteiro Maior que 5:");
+                n = int.Parse(Console.ReadLine());
+
+                if(n > 5)
+                {
+                    if (n % 2 == 0)
+                    {
+                        soma += n;
+                        par += n + ",";
+                        qtd++;
+                    }
+
+                    digitados += n + ",";
+                }
+            }
+            Console.WriteLine("Média dos Pares: {0}", soma /qtd);
+            Console.WriteLine("Números Pares Digitados: {0}", par);
+            Console.WriteLine("Números Digitados em geral: {0}", digitados);
+        }
+    }
+}
